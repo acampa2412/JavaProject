cab060135e19532395d9f8f62dd11a34c51c9c34
Anthony Campa <acampa2412@gmail.com>
2021-02-24 01:53:44 -0700

    Instagram project

---
+++ InstagramProject/InstagramProject/wwwroot/js/Index.js
@@ -0,0 +1,86 @@
+﻿var span = document.createElement("span");
+span.innerHTML = "My Gram";
+
+var div = document.getElementById("Container");
+
+div.appendChild(span);
+
+var loginButton = document.createElement("button");
+loginButtion.innerHTML = "Log In";
+
+div.appendChild(loginButton);
+
+var signUpButton = document.createElement("button");
+signUpButton.innerHTML = "Sign Up");
+
+div.appenChild(signUpButton);
+
+var lineBreak = document.createElement("br");
+
+div.appendChild(lineBreak);
+
+var image1 = document.createElement("span");
+image1.innerHTML = "Image 1";
+
+div.appendChild(image1);
+
+var image2 = document.createElement("span");
+image2.innerHTML = "Image 2";
+
+div.appendChild(image2);
+
+var image3 = document.createElement("span");
+image3.innerHTML = "Image 3";
+
+div.appendChild(image3);
+
+var image4 = document.createElement("span");
+image4.innerHTML = "Image 4";
+
+div.appendChild(image1Span);
+
+var image5 = document.createElement("span");
+image5.innerHTML = "Image 5";
+
+div.appendChild(image5);
+
+var lineBreak2 = document.createElement("br");
+
+div.appendChild(lineBreak2);
+
+var about = document.createElement("span");
+about.innerHTML = "About";
+
+div.appendChild(about);
+
+var blog = document.createElement("span");
+blog.innerHTML = "Blog";
+
+div.appendChild(blog);
+
+var jobs = document.createElement("span");
+jobs.innerHTML = "Jobs";
+
+div.appendChild(jobs);
+
+var help = document.createElement("span");
+help.innerHTML = "Help";
+
+div.appendChild(help);
+
+var lineBreak3 = document.createElement("br");
+
+div.appendChild(lineBreak3);
+
+var english = document.createElement("span");
+english.innerHTML = "English";
+
+div.appendChild(english);
+
+var year = document.createElement("span");
+year.innerHTML = "2021";
+
+div.appendChild(year);
+
+div.appendChild(span);
+
