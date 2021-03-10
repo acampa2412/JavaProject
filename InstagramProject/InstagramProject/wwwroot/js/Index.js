cb1ea354299f68b701b9a822c4580de8f06ce94c
Anthony Campa <acampa2412@gmail.com>
2021-03-10 01:06:05 -0700

    Date assignment

--- InstagramProject/InstagramProject/wwwroot/js/Index.js
+++ InstagramProject/InstagramProject/wwwroot/js/Index.js
@@ -3,5 +3,8 @@ var date
 
 var date = new Date(2028, 2, 28);
 
+var format = (date.getMonth() + 1).toString().padStart(2, "0") + "/"
+    + date.getDate() + "/";
+
 console.log(date);
 document.write(date.toLocaleString("zh-HK"))
